5c73385537c8546816f0b5c04bb4060e40711046
Tom Lokhorst <tom@lokhorst.eu>
2018-09-03 16:55:26 +0200

    Update Package.swift to version 4

--- Package.swift
+++ Package.swift
@@ -1,6 +1,14 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-  name: "XcodeEdit"
+  name: "XcodeEdit",
+  products: [
+    .library(name: "XcodeEdit", targets: ["XcodeEdit"]),
+  ],
+  targets: [
+    .target(name: "XcodeEdit"),
+  ],
+  swiftLanguageVersions: [4]
 )
 
